c72250c7d8b87bd4cbf4aff0f9cd66d247314a0c
Dan Bretl <danbretl@gmail.com>
2013-04-18 10:39:52 -0500

    Introduced the idea of a foreground scroll, separate from the background image scroll. Took dummy page text out of mock overlay image. Added per-page dummy text.

--- CrossFade/PLFScrollViewController.h
+++ CrossFade/PLFScrollViewController.h
@@ -11,15 +11,18 @@
 
 @interface PLFScrollViewController : UIViewController <UIScrollViewDelegate, UIGestureRecognizerDelegate>
 
-@property (nonatomic) float movementSuppressionFactor; // Defaults to 1.0. Forcibly kept >= 1.0. A factor of 1.0 will page content the full width of the screen. Factors greater than 1.0 will page content a fraction of that width.
+@property (nonatomic) float movementSuppressionFactor; // Defaults to 3.0. Forcibly kept >= 1.0. A factor of 1.0 will page content the full width of the screen. Factors greater than 1.0 will page content a fraction of that width.
 @property (nonatomic) float alphaChangeBufferPercentage; // Defaults to 0.0. Forcibly kept 0 <= p < 0.5. (If an attempt is made to set a value >= 0.5, a value of 0.48 will be set.) The larger the value, the longer the delay before changing alpha (and thus the quicker the alpha change once the change has begun).
 
 @property (nonatomic, weak) IBOutlet PLFClipView * clipView;
-@property (nonatomic, weak) IBOutlet UIScrollView * scrollView;
-@property (nonatomic, weak) IBOutlet UIScrollView * scrollViewTouch;
+@property (nonatomic, weak) IBOutlet UIScrollView * scrollViewBackground;
+@property (nonatomic, weak) IBOutlet UIScrollView * scrollViewForeground;
 
-@property (nonatomic, strong) NSArray * contentViews;
-@property (nonatomic, strong) UIView * contentViewsContainer;
+@property (nonatomic, strong) NSArray * backgroundContentViews;
+@property (nonatomic, strong) UIView * backgroundContentViewsContainer;
+
+@property (nonatomic, strong) NSArray * foregroundContentViews;
+@property (nonatomic, strong) UIView * foregroundContentViewsContainer;
 
 @property (nonatomic, strong) IBOutlet UIPanGestureRecognizer * movementSuppressionFactorPanGestureRecognizer;
 @property (nonatomic, strong) IBOutlet UIPanGestureRecognizer * alphaChangeBufferPercentagePanGestureRecognizer;
--- CrossFade/PLFScrollViewController.m
+++ CrossFade/PLFScrollViewController.m
@@ -12,8 +12,14 @@
 
 #define DEBUGGING_SCROLL_VIEW_CONTROLLER NO
 
+#define kMovementSuppressionFactorDefault 3.0
+#define kAlphaChangeBufferPercentageDefault 0.0
+
+
 @interface PLFScrollViewController () <MFMailComposeViewControllerDelegate>
-@property (nonatomic) NSArray * imageFilenames;
+@property (nonatomic) NSArray * imageFilenames; // Image filename strings
+@property (nonatomic) NSArray * imageHeadlines; // Headline text strings
+@property (nonatomic) NSArray * imageDescriptions; // Description text strings
 @property (nonatomic, readonly) CGFloat contentViewsSpacingValue;
 - (void)setControlLabelsTextToDataValues;
 - (IBAction)controlPanGesture:(UIPanGestureRecognizer *)gestureRecognizer;
@@ -39,9 +45,11 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.movementSuppressionFactor = 1.0;
-        self.alphaChangeBufferPercentage = 0.0;
+        self.movementSuppressionFactor = kMovementSuppressionFactorDefault;
+        self.alphaChangeBufferPercentage = kAlphaChangeBufferPercentageDefault;
         self.imageFilenames = @[@"test1.jpg", @"test2.jpg", @"test3.jpg", @"test4.jpg", @"test5.jpg", @"test6.jpg", @"test7.jpg", @"test8.jpg", @"test9.jpg", @"test10.jpg", @"test11.jpg"];
+        self.imageHeadlines = @[@"Store all your pictures", @"Automatically sync", @"Access anywhere", @"Send pictures easily"];
+        self.imageDescriptions = @[@"All backed up in one place", @"Never worry about losing pictures", @"View and organize on any device", @"Any picture can be sent instantly"];
     }
     return self;
 }
@@ -49,31 +57,71 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.overlayImageView.image = [[UIImage imageNamed:@"overlay"] resizableImageWithCapInsets:UIEdgeInsetsMake(200.0, 0, 200.0, 0)];
+    self.overlayImageView.image = [[UIImage imageNamed:@"overlay_sans_page_text"] resizableImageWithCapInsets:UIEdgeInsetsMake(200.0, 0, 200.0, 0)];
     
     self.controlsContainer.alpha = 0.0;
     [self setControlLabelsTextToDataValues];
         
-    self.contentViewsContainer = [[UIView alloc] initWithFrame:self.scrollView.bounds];
-    [self.scrollView addSubview:self.contentViewsContainer];
+    self.backgroundContentViewsContainer = [[UIView alloc] initWithFrame:self.scrollViewBackground.bounds];
+    [self.scrollViewBackground addSubview:self.backgroundContentViewsContainer];
     
-    self.clipView.scrollView = self.scrollView;
-    if (self.contentViews.count == 0) {
-        NSMutableArray * contentViews = [NSMutableArray array];
-        for (NSString * imageFilename in self.imageFilenames) {
+    self.foregroundContentViewsContainer = [[UIView alloc] initWithFrame:self.scrollViewForeground.bounds];
+    [self.scrollViewForeground addSubview:self.foregroundContentViewsContainer];
+    
+    self.clipView.scrollView = self.scrollViewBackground;
+    if (self.backgroundContentViews.count == 0) {
+        NSMutableArray * backgroundContentViews = [NSMutableArray array];
+        NSMutableArray * foregroundContentViews = [NSMutableArray array];
+        for (int i=0; i<self.imageFilenames.count; i++) {
+            NSString * imageFilename = self.imageFilenames[i];
+            
             UIImageView * contentView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageFilename]];
             contentView.contentMode = UIViewContentModeScaleAspectFill;
-            [self.contentViewsContainer addSubview:contentView];
-            [self.contentViewsContainer sendSubviewToBack:contentView];
-            [contentViews addObject:contentView];
+            [self.backgroundContentViewsContainer addSubview:contentView];
+            [self.backgroundContentViewsContainer sendSubviewToBack:contentView];
+            [backgroundContentViews addObject:contentView];
+            
+            UIView * contentViewForeground = [[UIView alloc] initWithFrame:CGRectOffset(self.scrollViewForeground.bounds, [self.imageFilenames indexOfObject:imageFilename] * self.scrollViewForeground.bounds.size.width, 0)];
+            [self.foregroundContentViewsContainer addSubview:contentViewForeground];
+            [foregroundContentViews addObject:contentViewForeground];
+            
+            UILabel * contentViewHeadlineLabel = [[UILabel alloc] initWithFrame:CGRectMake(25.0, contentViewForeground.bounds.size.height - 160.0, contentViewForeground.bounds.size.width - 25.0 * 2, 28.0)];
+            contentViewHeadlineLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+            contentViewHeadlineLabel.textColor = [UIColor whiteColor];
+            contentViewHeadlineLabel.font = [UIFont boldSystemFontOfSize:20.0];
+            contentViewHeadlineLabel.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.5];
+            contentViewHeadlineLabel.shadowOffset = CGSizeMake(0, 1.0);
+            contentViewHeadlineLabel.textAlignment = NSTextAlignmentCenter;
+            contentViewHeadlineLabel.text = self.imageHeadlines[i % self.imageHeadlines.count];
+            UILabel * contentViewDescriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(25.0, CGRectGetMaxY(contentViewHeadlineLabel.frame), contentViewForeground.bounds.size.width - 25.0 * 2, 28.0)];
+            contentViewDescriptionLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin;
+            contentViewDescriptionLabel.textColor = [UIColor whiteColor];
+            contentViewDescriptionLabel.font = [UIFont systemFontOfSize:16.0];
+            contentViewDescriptionLabel.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.5];
+            contentViewDescriptionLabel.shadowOffset = CGSizeMake(0, 1.0);
+            contentViewDescriptionLabel.textAlignment = NSTextAlignmentCenter;
+            contentViewDescriptionLabel.numberOfLines = 2;
+            contentViewDescriptionLabel.text = self.imageDescriptions[i % self.imageDescriptions.count];
+            if (DEBUGGING_SCROLL_VIEW_CONTROLLER) {
+                contentViewHeadlineLabel.backgroundColor = [UIColor redColor];
+                contentViewDescriptionLabel.backgroundColor = [UIColor yellowColor];
+            } else {
+                contentViewHeadlineLabel.backgroundColor = [UIColor clearColor];
+                contentViewDescriptionLabel.backgroundColor = [UIColor clearColor];
+            }
+            
+            [contentViewForeground addSubview:contentViewHeadlineLabel];
+            [contentViewForeground addSubview:contentViewDescriptionLabel];
+            
         }
-        self.contentViews = contentViews;
+        self.backgroundContentViews = backgroundContentViews;
+        self.foregroundContentViews = foregroundContentViews;
     }
     
-    self.scrollViewTouch.contentOffset = CGPointZero;
+    self.scrollViewForeground.contentOffset = CGPointZero;
     
-    self.scrollView.showsHorizontalScrollIndicator = DEBUGGING_SCROLL_VIEW_CONTROLLER;
-    self.scrollView.showsVerticalScrollIndicator = DEBUGGING_SCROLL_VIEW_CONTROLLER;
+    self.scrollViewBackground.showsHorizontalScrollIndicator = DEBUGGING_SCROLL_VIEW_CONTROLLER;
+    self.scrollViewBackground.showsVerticalScrollIndicator = DEBUGGING_SCROLL_VIEW_CONTROLLER;
     
 }
 
@@ -93,35 +141,37 @@
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
     
-    self.scrollViewTouch.frame = self.view.bounds;
-    self.scrollViewTouch.contentSize = CGSizeMake(self.view.bounds.size.width * self.imageFilenames.count, self.scrollViewTouch.frame.size.height);
-    self.scrollViewTouch.backgroundColor = [UIColor clearColor];
+    self.scrollViewForeground.frame = self.view.bounds;
+    self.scrollViewForeground.contentSize = CGSizeMake(self.view.bounds.size.width * self.imageFilenames.count, self.scrollViewForeground.frame.size.height);
+    self.scrollViewForeground.backgroundColor = [UIColor clearColor];
 
-    self.scrollView.frame = CGRectMake((self.view.bounds.size.width - self.contentViewsSpacingValue) / 2.0, 0, self.contentViewsSpacingValue, self.view.bounds.size.height);
+    self.scrollViewBackground.frame = CGRectMake((self.view.bounds.size.width - self.contentViewsSpacingValue) / 2.0, 0, self.contentViewsSpacingValue, self.view.bounds.size.height);
     
-    for (int i=0; i<self.contentViews.count; i++) {
-        UIView * contentView = self.contentViews[i];
-        contentView.frame = CGRectMake(self.contentViewsSpacingValue * i, 0, self.view.bounds.size.width + self.contentViewsSpacingValue * 2.0, self.scrollView.bounds.size.height);
+    for (int i=0; i<self.imageFilenames.count; i++) {
+        UIView * contentView = self.backgroundContentViews[i];
+        contentView.frame = CGRectMake(self.contentViewsSpacingValue * i, 0, self.view.bounds.size.width + self.contentViewsSpacingValue * 2.0, self.scrollViewBackground.bounds.size.height);
         NSLog(@"contentView(%d).frame = %@", i, NSStringFromCGRect(contentView.frame));
+        UIView * contentViewForeground = self.foregroundContentViews[i];
+        contentViewForeground.frame = CGRectMake(self.scrollViewForeground.bounds.size.width * i, 0, self.scrollViewForeground.bounds.size.width, self.scrollViewForeground.bounds.size.height);
     }
     
-    self.contentViewsContainer.frame = CGRectMake(-(self.scrollView.frame.origin.x + self.scrollView.frame.size.width), 0, CGRectGetMaxX(((UIView *)self.contentViews.lastObject).frame), self.scrollView.bounds.size.height);
+    self.backgroundContentViewsContainer.frame = CGRectMake(-(self.scrollViewBackground.frame.origin.x + self.scrollViewBackground.frame.size.width), 0, CGRectGetMaxX(((UIView *)self.backgroundContentViews.lastObject).frame), self.scrollViewBackground.bounds.size.height);
     
-    self.scrollView.contentSize = CGSizeMake(self.contentViewsSpacingValue * self.contentViews.count, self.contentViewsContainer.frame.size.height);
-    NSLog(@"%@ self.scrollView.contentSize = %@", NSStringFromSelector(_cmd), NSStringFromCGSize(self.scrollView.contentSize));
-    self.scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, -self.scrollView.frame.origin.x, 0, -(self.view.bounds.size.width - CGRectGetMaxX(self.scrollView.frame)));
+    self.scrollViewBackground.contentSize = CGSizeMake(self.contentViewsSpacingValue * self.backgroundContentViews.count, self.backgroundContentViewsContainer.frame.size.height);
+    NSLog(@"%@ self.scrollView.contentSize = %@", NSStringFromSelector(_cmd), NSStringFromCGSize(self.scrollViewBackground.contentSize));
+    self.scrollViewBackground.scrollIndicatorInsets = UIEdgeInsetsMake(0, -self.scrollViewBackground.frame.origin.x, 0, -(self.view.bounds.size.width - CGRectGetMaxX(self.scrollViewBackground.frame)));
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    if (scrollView == self.scrollViewTouch) {
-        self.scrollView.contentOffset = CGPointMake(self.scrollViewTouch.contentOffset.x / self.movementSuppressionFactor, self.scrollViewTouch.contentOffset.y);
-    } else if (scrollView == self.scrollView) {
-        CGFloat contentOffsetX = self.scrollView.contentOffset.x;
+    if (scrollView == self.scrollViewForeground) {
+        self.scrollViewBackground.contentOffset = CGPointMake(self.scrollViewForeground.contentOffset.x / self.movementSuppressionFactor, self.scrollViewForeground.contentOffset.y);
+    } else if (scrollView == self.scrollViewBackground) {
+        CGFloat contentOffsetX = self.scrollViewBackground.contentOffset.x;
         CGFloat scrollViewWidth = self.contentViewsSpacingValue;
-        CGFloat contentOffsetCenterX = contentOffsetX + scrollViewWidth / 2.0 - self.contentViewsContainer.frame.origin.x;
+        CGFloat contentOffsetCenterX = contentOffsetX + scrollViewWidth / 2.0 - self.backgroundContentViewsContainer.frame.origin.x;
 //        NSLog(@"coc.x = %f", contentOffsetCenterX);
         CGFloat d = 0;
-        for (UIView * contentView in self.contentViews) {
+        for (UIView * contentView in self.backgroundContentViews) {
             CGFloat cxd = self.contentViewsSpacingValue;
             CGFloat cxFull = contentView.center.x + cxd * self.alphaChangeBufferPercentage;
             CGFloat cxZero = contentView.center.x + self.contentViewsSpacingValue - cxd * self.alphaChangeBufferPercentage;
@@ -129,7 +179,7 @@
             d = contentOffsetCenterX - cxFull;
             d = MAX(0, d);
             d = MIN(d, cxd);
-            if (contentView == self.contentViews.lastObject)
+            if (contentView == self.backgroundContentViews.lastObject)
                 d = 0;
             CGFloat a = 1.0 - (d / cxd);
 //            NSLog(@"  cv%d", [self.contentViews indexOfObject:contentView]);
@@ -147,15 +197,15 @@
 - (void)setMovementSuppressionFactor:(float)movementSuppressionFactor {
     movementSuppressionFactor = MAX(1.0, movementSuppressionFactor);
     if (_movementSuppressionFactor != movementSuppressionFactor) {
-        int currentIndex = roundf(self.scrollViewTouch.contentOffset.x / self.scrollViewTouch.bounds.size.width);
+        int currentIndex = roundf(self.scrollViewForeground.contentOffset.x / self.scrollViewForeground.bounds.size.width);
         currentIndex = MAX(0, currentIndex);
         currentIndex = MIN(currentIndex, self.imageFilenames.count);
         _movementSuppressionFactor = movementSuppressionFactor;
         if (self.isViewLoaded && self.view.window) {
             [self setControlLabelsTextToDataValues];
             [self viewDidLayoutSubviews];
-            self.scrollViewTouch.contentOffset = CGPointZero; // Not sure why this was necessary.
-            self.scrollViewTouch.contentOffset = CGPointMake(currentIndex * self.scrollViewTouch.bounds.size.width, self.scrollViewTouch.contentOffset.y);
+            self.scrollViewForeground.contentOffset = CGPointZero; // Not sure why this was necessary.
+            self.scrollViewForeground.contentOffset = CGPointMake(currentIndex * self.scrollViewForeground.bounds.size.width, self.scrollViewForeground.contentOffset.y);
         }
     }
 }
@@ -192,12 +242,12 @@
     [UIView animateWithDuration:animatedToStart ? 0.15 : 0.0 delay:0.0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
         self.controlsContainer.alpha = 1.0;
     } completion:^(BOOL finished) {
-        [self performSelector:@selector(setControlLabelsInvisibleIfUntouched) withObject:nil afterDelay:5.0];
+        [self performSelector:@selector(setControlLabelsInvisibleIfUntouched) withObject:nil afterDelay:DEBUGGING_SCROLL_VIEW_CONTROLLER ? 0.5 : 5.0];
     }];
 }
 
 - (CGFloat) contentViewsSpacingValue {
-    return self.scrollViewTouch.bounds.size.width / self.movementSuppressionFactor;
+    return self.scrollViewForeground.bounds.size.width / self.movementSuppressionFactor;
 }
 
 - (float)getControlValueForGestureRecognizer:(UIPanGestureRecognizer *)gestureRecognizer {
